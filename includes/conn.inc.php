a6a52aa8108bd4c472e567430d39c70a729726e9
simon <simon@dokupit.com>
2022-07-06 22:01:50 +0200

    commented functions

--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -1,14 +1,25 @@
 <?php
 require "config.inc.php";
 
+/**
+ * Erstellt eine instanz einer Mysqli klasse
+ * Wenn die verbindung nicht erfolgreich wird der error gelogged
+ * @return $conn = die erstellte connection
+ */
 function openConn() {
     $conn = new Mysqli(DB["host"],DB["user"],DB["pwd"],DB["db"]);
-
     if ($conn->connect_errno > 0) {
-        die ("Fehler im Verbindungsaufbau " . $conn->connect_error);
+        die ("Fehler im Verbindungsaufbau " . error_log($conn->connect_error));
     }
     return $conn;
 };
 
+/**
+ * Schließt die connection zu DB
+ * @param $conn = eine Mysqli instanz
+ */
+function closeConn($conn) {
+    $conn->close();
+}
 
 ?>
